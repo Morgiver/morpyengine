6f5bb33cb6f17025d943d541ff374d39b04bd8f2
Morgiver <morgan@morgiver.net>
2021-04-27 11:10:53 +0200

    Create sketch-example.py, create Sketch class

---
+++ sketch-example.py
@@ -0,0 +1,13 @@
+
+from Engine import Engine
+
+
+class Sketch(Engine):
+    def __init__(self):
+        Engine.__init__(self, window_title="My Sketch", width=600, height=600)
+
+    def setup(self):
+        pass
+
+    def update(self):
+        pass
