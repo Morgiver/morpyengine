29dd7c580ecb931176d890ee2f0fd80f7e1525c2
Morgiver <morgan@morgiver.net>
2021-04-27 10:18:26 +0200

    Add setup, update methods to be overridded, adding handle_events and get_surface methods

--- Engine.py
+++ Engine.py
@@ -11,6 +11,21 @@ class Engine:
         self.loop_state = LOOP_STATE_STOPPED
         self.window = None
 
+    def setup(self):
+        pass
+
+    def update(self):
+        pass
+
     def set_loop_state(self, state):
         if state == LOOP_STATE_RUNNING or state == LOOP_STATE_STOPPED or state == LOOP_STATE_PAUSED:
             self.loop_state = state
+
+    def handle_events(self, events):
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                self.set_loop_state(LOOP_STATE_STOPPED)
+                break
+
+    def get_surface(self):
+        return self.window.get_surface()
