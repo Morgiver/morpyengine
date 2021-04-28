f17b1173940a81410ec4515a33294f00d58b6057
Morgiver <morgan@morgiver.net>
2021-04-28 08:50:25 +0200

    Updating project url in setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setuptools.setup(
     description="A small SDL2 Graphic Engine to learn how it work",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Morgiver/graphic-engine",
+    url="https://github.com/Morgiver/morpyengine",
     project_urls={
-        "Bug Tracker": "https://github.com/Morgiver/graphic-engine/issues",
+        "Bug Tracker": "https://github.com/Morgiver/morpyengine/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
